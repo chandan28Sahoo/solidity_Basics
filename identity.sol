a5823394c0fd8c1f23f00d823ece92894b7f6523
"Chandan Sahoo" <"chandan19@navgurukul.org">
2022-06-24 21:00:15 +0530

    add file

---
+++ identity.sol
@@ -0,0 +1,21 @@
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity >= 0.5.0 < 0.9.0;
+//pragma solidity >=0.8.2 < 0.9.0;
+
+contract Identity{
+    string name;
+    uint age;
+    constructor() {
+        name = "Chandan";
+        age = 17;
+    }
+    function getName() view public returns(string memory){
+        return name;
+    }
+    function getAge() view public returns(uint){
+        return age;
+    }
+    function setAge() public{
+        age = age + 1;
+    }
+}
---
+++ state.sol
@@ -0,0 +1,24 @@
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity >= 0.5.0 < 0.9.0;
+
+// contract State{
+//     //state variable
+//     uint public age;
+// /*     constructor() {
+//         age = 20;
+//     }
+//  */
+//     function setAge() public {
+//         age = age + 20;
+//     }
+// }
+
+contract Local{
+    //local variable : data store in  stack storage;
+    // for string alsway store in contract level; and it has default has memory. if you want to use inside function then use memory key word;
+    // memory key word can not use contract level. 
+    function store() pure public returns(uint){
+        uint age = 20;
+        return age;
+    }
+}
